cb82c65e6aa130bcb943edfed79a9c755bbcefd0
ujjwal-malik-um <152059523+ujjwal-malik-um@users.noreply.github.com>
2023-12-05 13:04:50 +0530

    Update Retail Marketing Analysis.py

--- Retail Marketing Analysis.py
+++ Retail Marketing Analysis.py
@@ -3,47 +3,52 @@ import pandas as pd
 import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
+import matplotlib.pyplot as plt
+
 df = pd.read_csv('marketing_data.csv')
+
 df.head(5)
 df.info()
 df.describe()
-df.isna().sum()
-df=df.dropna()
-import re
-if(bool(re.match('^[a-zA-Z0-9]*$',o))==True):
-    print("valid")
-else:
-    print("invalid")
-#create our dictionary, shopping items as keys and price of items as values
-shoppingDict = {"Apple":40.00,"Banana":30.00,"Fish":100.00,"Bread":45.00,"Milk":20.00}
-#iterate through the dictionaries items printing them out in a certain format
-for k,v in shoppingDict.items():print(f'{k}: Php {v}')
-while True:
-    #prompt user to enter an item
-    choice=input('\nChoose an item: Apple, Banana, Fish, Bread, Milk\n')
-    #check if that item is found in the dictionary
-    #if so add the key's value(price) to price
-    try:price += shoppingDict.get(choice)
-    #if the user does not enter a valid item, we will print error
-    except KeyError:print('Error')
-    #ask them if they want to play again
-    if input('Would you like to go again? y/n') == 'n':
-        print(f'Total Cost: {price}')
-        break
-from scipy.stats.stats import pearsonr
-#calculation correlation coefficient and p-value between x and y
-pearsonr(x, y)
-import seaborn
-import matplotlib.pyplot as plt
-############# Main Section ############
-# loading dataset using seaborn
-# pairplot with hue sex
-seaborn.pairplot(df, hue='Response')
-# to show
+
+#check for and handle missing values
+df.isnull().sum()/len(df)*100
+df=df.dropna(inplace=True)
+
+#handle special variables
+df[' Income '] = df[' Income '].str.replace('$','').str.replace(',','')
+
+#find age
+df['Age'] = df['Year_Birth'].apply(lambda x: 2022-x)
+
+#find total amount spent on products
+df['Total_amount_spent'] = np.sum(df.filter(regex='Mnt'), axis=1)
+
+#find total purchases made
+df['TotalPurchases'] = np.sum(df.filter(regex='Purchases'),axis=1)
+
+#visualize which campaign is most successful
+cmp = df.filter(regex='Cmp').sum()
+plt.pie(cmp,autopct='%0.2f',labels=cmp.index,explode=[0,0.2,0,0,0])
 plt.show()
-seaborn.pairplot(df, hue='Education')
-# to show
+
+#Visualize (Bar Chart) the distribution of the age with respect to customers who accepted the last campaign
+customers_accepted = df[df['Response']==1]
+sns.displot(customers_accepted['Age'])
 plt.show()
-seaborn.pairplot(df, hue='Marital Status')
-# to show
+
+#Visualize (Box Plot) the distribution of Total amount spent Vs Dependents. (Dependents=['Kidhome']+['Teenhome'])
+df["Dependents"] = df["Kidhome"] + df["Teenhome"]
+
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+sns.boxplot(y=df["Total_amount_spent"],x=df["Dependents"])
+plt.subplot(1,2,2)
+sns.boxplot(y=df["TotalPurchases"],x=df["Dependents"])
+plt.show()
+
+#Perform Correlation Analysis (Heat Map)
+dff = df.drop('ID',axis=1)
+plt.figure(figsize=[18,7])
+sns.heatmap(dff.corr(),annot=True,cmap='viridis')
 plt.show()
